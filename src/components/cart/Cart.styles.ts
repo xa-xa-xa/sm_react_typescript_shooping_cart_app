8c88ddfeb044f2ab0b2907b7307a69f25839d289
Alex K <askrasn@gmail.com>
2021-01-27 13:55:58 -0800

    initial commit

--- src/components/cart/Cart.styles.ts
+++ src/components/cart/Cart.styles.ts
@@ -4,4 +4,10 @@ export const Wrapper = styled.aside`
   font-family: Arial, Helvetica, sans-serif;
   width: 450px;
   padding: 1rem;
+  .closeIcon {
+    float: right;
+    :hover {
+      cursor: pointer; 
+    }
+  }
 `;
--- src/components/cartItem/CartItem.tsx
+++ src/components/cartItem/CartItem.tsx
@@ -1,6 +1,7 @@
 import { CartItemType } from '../../types';
 import { Wrapper } from './CartItem.styles';
 import { Button } from '@material-ui/core';
+import { Add, Remove } from '@material-ui/icons';
 
 type Props = {
   item: CartItemType;
@@ -12,32 +13,38 @@ type Props = {
 function CartItem({ item, addToCart, removeFromCart }: Props) {
   return (
     <Wrapper>
-      <div>
+      <div className='container'>
         <h3>{item.title}</h3>
-        <div className='information'>
-          <p>Price: ${item.price}</p>
-          <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+        <div className='info-container'>
+          <section className='info'>
+            <div className='information'>
+              <p>Price: ${item.price}</p>
+              <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+            </div>
+            <div className='buttons'>
+              <Button
+                size='small'
+                disableElevation
+                variant='contained'
+                onClick={() => removeFromCart(item.id)}
+              >
+                <Remove />
+              </Button>
+              <p>{item.amount}</p>
+              <Button
+                size='small'
+                disableElevation
+                variant='contained'
+                onClick={() => addToCart(item)}
+              >
+                <Add />
+              </Button>
+            </div>
+          </section>
+          <section className='image'>
+            <img src={item.image} alt={item.title} />
+          </section>
         </div>
-        <div className='buttons'>
-          <Button
-            size='small'
-            disableElevation
-            variant='contained'
-            onClick={() => removeFromCart(item.id)}
-          >
-            -
-          </Button>
-          <p>{item.amount}</p>
-          <Button
-            size='small'
-            disableElevation
-            variant='contained'
-            onClick={() => addToCart(item)}
-          >
-            +
-          </Button>
-        </div>
-        <img src={item.image} alt={item.title} />
       </div>
     </Wrapper>
   );
